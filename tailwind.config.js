d4dca2b165bae722d762481bb18921cbe4d1febc
Chris M. <crismartin994@gmail.com>
2024-07-28 16:20:23 -0400

    Setup tailwind css and reorganized files

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
+/* 
+Add the @tailwind directives for each of Tailwind’s layers to your main CSS file.
+
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+*/
