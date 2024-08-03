31dac36129b1662a21dad247b92cd92fb8713b08
Chris M. <crismartin994@gmail.com>
2024-08-03 14:09:17 -0400

    Implemented Navbar

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,15 +1,20 @@
+import React from 'react';
+import Navbar from '../navbar';
+
+
 export default function Homepage() {
   return (
     /* This is the main page, anything that is used everywhere  
         goes in here, otherwise create a page.
     */
     <div>
-      <h1 className="text-center container text-3xl font-bold underline">
+      <Navbar />
+      <h1 className="text-center text-3xl font-bold underline">
         
         Hello world!
         <p>Help</p>
       </h1>
-      <h2 className="text-center container">
+      <h2 className="text-center">
         this is a test?
       </h2>
     </div>
